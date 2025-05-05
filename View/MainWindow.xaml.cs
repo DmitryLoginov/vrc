bb21d5f3a794105870f94d69ca2754cdc8da3cee
DmitryLoginov <daimom25@gmail.com>
2025-05-05 21:29:42 +0300

    ui: implemented basic UI

--- View/MainWindow.xaml.cs
+++ View/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using OfficeOpenXml;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,36 @@ namespace View
     /// </summary>
     public partial class MainWindow : Window
     {
+        private IList<ExcelPackage> _files;
+        
         public MainWindow()
         {
             InitializeComponent();
+
+            _files = new List<ExcelPackage>();
+        }
+
+        private void button_addFile_Click(object sender, RoutedEventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() ?? false)
+            {
+                var package = new ExcelPackage(ofd.FileName);
+                listBox_files.Items.Add(ofd.FileName);
+            }
+        }
+
+        private void button_removeFile_Click(object sender, RoutedEventArgs e)
+        {
+            if (listBox_files.SelectedItem == null)
+            {
+                e.Handled = true;
+                return;
+            }
+            else
+            {
+                //_files.FirstOrDefault(item => item.File.)
+            }
         }
     }
 }
